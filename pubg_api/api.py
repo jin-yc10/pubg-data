bcb7958d098f7bb4d70a4bfbedc6d79af8f6f9ba
jin-yc10 <ccjinyc@gmail.com>
2017-09-19 13:28:08 -0400

    add api test code

--- pubg_api/api.py
+++ pubg_api/api.py
@@ -4,3 +4,13 @@ def get_key(PATH):
         key = k.readline()
     return key
 
+if __name__ == '__main__':
+    k = get_key('../.PRIVATE')
+    import requests, json
+    url = 'https://pubgtracker.com/api/profile/pc/Yhyoek'
+    headers = {
+        'TRN-API-KEY': k
+    }
+    r = requests.get(url, headers=headers)
+    user = json.loads(r.text)
+    print(user['PlayerName'])
