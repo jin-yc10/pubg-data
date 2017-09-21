736ee973a37a4ab7c16e08e58a74235ebec04c83
jin-yc14 <ccjinyc@gmail.com>
2017-09-21 15:01:03 +0800

    list user

--- list_user.py
+++ list_user.py
@@ -5,6 +5,6 @@ user_db = leveldb.LevelDB('./user_db')
 cnt = 0;
 for k,v in user_db.RangeIter():
     obj = json.loads(v.decode('utf8'))
-    print(cnt, k, obj['PlayerName'])
+    #print(cnt, k, obj['PlayerName'])
     cnt += 1
 print cnt
